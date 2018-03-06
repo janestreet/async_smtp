c65667e0044d115c44b1d2bb12dbd46436967ea5
Thomas Refis <thomas.refis@gmail.com>
2018-03-06 10:37:08 +0000

    v0.10.116.16+14

--- tools/src/log_selector.mli
+++ tools/src/log_selector.mli
@@ -23,7 +23,7 @@ type t = Base.t Blang.t [@@deriving sexp]
     - The empty conjunction list (top) is equivalent to [True]
     - A disjunction containing a [Not] is replaced by [True]
     - An empty disjuction list (middle) is equivalent to [False], and the whole
-    cnf will be rewritten to [ [ ] ].
+      cnf will be rewritten to [ [ ] ].
 *)
 val permissive_cnf : t -> Base.t list list
 
